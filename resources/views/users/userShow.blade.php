070131ae837ff1debbee4e930fb39c659149a65d
pravedens <107057525+pravedens@users.noreply.github.com>
2024-08-09 18:58:41 +0500

    Get Data_1

---
+++ resources/views/users/userShow.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            Users
+        </h2>
+    </x-slot>
+
+    <div class="w-full px-4 pt-10 sm:px-6 md:px-8 lg:ps-72">
+        <header>
+            <h1>User Show {{ $id }}</h1>
+        </header>
+    </div>
+</x-app-layout>
