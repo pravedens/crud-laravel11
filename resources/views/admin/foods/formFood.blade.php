c147dc955f4176d74937f68b97d78e81cb4fe7d1
pravedens <107057525+pravedens@users.noreply.github.com>
2024-10-01 07:21:23 +0500

    TinyMCe

--- resources/views/admin/foods/formFood.blade.php
+++ resources/views/admin/foods/formFood.blade.php
@@ -81,7 +81,7 @@
                         </div>
 
                         <div class="sm:col-span-9">
-                            <textarea
+                            <textarea name="description" id="description"
                                 class="block w-full px-4 py-3 text-sm border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                                 rows="3" placeholder="Description food..."></textarea>
                         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,24 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"
+        integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
-        $(document).ready(function () {
-            $('#currency').mask('#.##0', {reverse: true});
+        $(document).ready(function() {
+            $('#currency').mask('#.##0', {
+                reverse: true
+            });
         })
+    </script>
+
+    <script src="https://cdn.tiny.cloud/1/qi89imit2i0afui8nnp59io5555teo5bvsn4x7wommzvi0go/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea#description', // Replace this CSS selector to match the placeholder element for TinyMCE
+            plugins: 'code table lists',
+            toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+        });
     </script>
 
 </head>
@@ -41,8 +54,8 @@
         <!-- Page Content -->
         <main>
             @if (session('success'))
-                <div class="p-4 mt-2 text-center bg-teal-500 rounded-lg placeholder:white text--sm" role="alert" tabindex="-1"
-                    aria-labelledby="hs-solid-color-success-label">
+                <div class="p-4 mt-2 text-center bg-teal-500 rounded-lg placeholder:white text--sm" role="alert"
+                    tabindex="-1" aria-labelledby="hs-solid-color-success-label">
                     <span id="hs-solid-color-success-label" class="font-bold">Success</span> {{ session('success') }}
                 </div>
             @endif
